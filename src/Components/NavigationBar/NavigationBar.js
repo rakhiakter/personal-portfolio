d3cc0ccef306fd63db775317e79899ae6f27f5e0
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-10-29 23:17:54 +0600

    navigation done

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import './NavigationBar.css'; 
+
 
 const NavigationBar = () => {
     return (
-      
-        <Container>
-          <Navbar variant="light">
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-           
-          </Navbar>
-        </Container>
-     
+      <Container>
+        <Navbar variant="light">
+          <Navbar.Brand id="logo" href="#home">R</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link className="clr" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="clr" href="#features">
+              About
+            </Nav.Link>
+            <Nav.Link className="clr" href="#pricing">
+              Projects
+            </Nav.Link>
+            <Nav.Link className="clr" href="#pricing">
+              Blog
+            </Nav.Link>
+            <Nav.Link className="clr" href="#pricing">
+              Contact
+            </Nav.Link>
+          </Nav>
+          <Button variant="outline-dark">Resume</Button>
+        </Navbar>
+      </Container>
     );
 };
 
